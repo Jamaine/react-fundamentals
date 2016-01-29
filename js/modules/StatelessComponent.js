ada3da2e12172c97b11a8e292807e2a64bbe5bf0
Jamaine Spooner <jamaine.spooner@gmail.com>
2016-01-29 14:41:09 +0000

    React Fundamentals: Hello World - First Component

---
+++ js/modules/StatelessComponent.js
@@ -0,0 +1,6 @@
+const React = require('react')
+
+// Stateless function component - cannot have state
+const SFC = () => <h1>Ta backfoot</h1>
+
+module.exports = SFC;
--- js/modules/app.js
+++ js/modules/app.js
@@ -1,11 +1,12 @@
 const React = require('react');
 
-// class App extends React.Component {
-//   render () {
-//     return (
-//       <h1>hello world!!!</h1>
-//     )
-//   }
-// }
-const App = () => <h1>ta backside!!</h1>
+// Stateful component - it can have state
+class App extends React.Component {
+  render () {
+    return (
+      <h1>hello world!!!</h1>
+    )
+  }
+}
+
 module.exports = App;
