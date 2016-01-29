6bb42d60f9fde0eb99305aa20b961c6326ee1db0
Jamaine Spooner <jamaine.spooner@gmail.com>
2016-01-29 13:31:49 +0000

    added new files and updated index.html

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const App = require('./modules/app.js')
+
+ReactDom.render(<App/>,document.querySelector('.react'))
---
+++ js/modules/app.js
@@ -0,0 +1,11 @@
+const React = require('react');
+
+// class App extends React.Component {
+//   render () {
+//     return (
+//       <h1>hello world!!!</h1>
+//     )
+//   }
+// }
+const App = () => <h1>ta backside!!</h1>
+module.exports = App;
